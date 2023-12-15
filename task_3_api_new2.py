0fcbe38c0319fbdc4704147870d5488489d334d5
Maxim Lastin <lastinm@mail.ru>
2023-12-16 00:37:55 +0300

    Ch task_3_api

--- task_3_api_new2.py
+++ task_3_api_new2.py
@@ -1,8 +1,6 @@
 from sre_parse import Tokenizer
 from fastapi import FastAPI                 # Библиотека для создания веб-приложения
-#from transformers import pipeline           # Пайплайн модели машинного обучения
 from pydantic import BaseModel              # Класс для интерфейса веб-приложения
-#import re                                   # Библиотека регулярных выражений
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
@@ -19,37 +17,20 @@ model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
 if torch.cuda.is_available():
     model.cuda()
 
-# Создадим функцию вызова пайплайна
-# def load_model(model_checkpoint):
-#     model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
-#     if torch.cuda.is_available():
-#         model.cuda()
-#     return model_checkpoint, model
-
-# Загружаем предварительно обученную модель в переменную model
-#model = load_model(model_checkpoint)
-
 # Для корневой страницы сайта выдадим предупреждение.
 @app.get("/")
-async def root():
+def root():
     """Для получения результата от модели используйте в адресе /predict/"""
     return {"msg": "Для получения результата от модели используйте в адресе /predict/"}
 
 # Основная функция приложения, вызываемая по адресу: http://....../predict/
 # Получаем из запроса POST объект item созданного нами класса Item
 @app.post("/predict/")
-async def predict(item: Item):
+def predict(item: Item):
     u"""Сформулируйте строку для определения вероятности содержания в ней негативного подтекста."""
     with torch.no_grad():
         inputs = tokenizer(item.text, return_tensors='pt', truncation=True, padding=True).to(model.device)
         proba = torch.sigmoid(model(**inputs).logits).cpu().numpy()
     if isinstance(item.text, str):
         proba = proba[0]
-    #if aggregate:
     return 1 - proba.T[0] * (1 - proba.T[-1])
-    #return proba
-
-
-
-
-
