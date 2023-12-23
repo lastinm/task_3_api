8189d66764bd4b9d3d807fbc0aac8c47bb7446e0
Maxim Lastin <lastinm@mail.ru>
2023-12-23 10:58:51 +0300

    Changed curl scriprt

--- curl_post_local_predict.sh
+++ curl_post_local_predict.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl -X 'POST' 'http://127.0.0.1:8000/predict/' -H 'Content-Type: application/json' -d '{ "text": "Это наглый нигер."}'
+
+# toxic
+curl -X 'POST' 'http://127.0.0.1:8000/predict/' -H 'Content-Type: application/json' -d '{ "text": "Эта лодка дырявая калоша."}'
+
+# non-toxic
+#curl -X 'POST' 'http://127.0.0.1:8000/predict/' -H 'Content-Type: application/json' -d '{ "text": "Съешь еще спелых яблочек."}'
